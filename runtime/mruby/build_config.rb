c81bdf8cd9be65d2f4df247ff4ffcea6af1c4b08
Adam Moussa Ali <adammusaaly@gmail.com>
2026-08-25 17:58:10 -0400

    fix: build only portable mruby executables

--- runtime/mruby/build_config.rb
+++ runtime/mruby/build_config.rb
@@ -2,7 +2,11 @@
 
 MRuby::Build.new("zui") do |configuration|
   configuration.toolchain
-  configuration.gembox "default"
+  %w[stdlib stdlib-ext stdlib-io math metaprog].each do |gembox|
+    configuration.gembox gembox
+  end
+  configuration.gem core: "mruby-bin-mrbc"
+  configuration.gem core: "mruby-bin-mruby"
   configuration.gem ENV.fetch("ZUI_MRUBY_JSON")
 
   unless RUBY_PLATFORM.match?(/mswin|mingw|cygwin/i)
