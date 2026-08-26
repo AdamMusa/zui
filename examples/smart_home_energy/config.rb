2b2d71f657390444a10b62ede8c7d753750f2464
AdamMusa <adammusa2222@gmail.com>
2026-08-25 20:39:22 -0400

    ...

---
+++ examples/smart_home_energy/config.rb
@@ -0,0 +1,16 @@
+Zui::Dist.configure do
+  name "Smart Home"
+  identifier "dev.zui.nebula-command"
+  version "0.1.0"
+
+  publisher "Zui Project"
+  description "A smart home energy dashboard."
+  license "MIT"
+  homepage "https://github.com/AdamMusa/zui"
+
+  icon linux: "assets/ruby.jpg",
+        macos: "assets/ruby.icns",
+        windows: "assets/ruby.jpg"
+
+  categories "Utility", "Development"
+end
